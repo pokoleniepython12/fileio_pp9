dc08807fe91d74b254185629431174fd287da057
Екатерина <katrindenev23@yandex.com>
2024-11-26 09:47:28 +0300

    Определена функция для отправки запроса с файлом в теле запроса. Полученная ссылка вставляется в поле ввода entry.

--- main_fileio.py
+++ main_fileio.py
@@ -4,7 +4,14 @@ from tkinter import filedialog as fd
 from tkinter import ttk
 
 def upload():
-    pass
+    filepath=fd.askopenfilename()
+    if filepath:
+        files={"file":open(filepath, "rb")}
+        response=requests.post('https://file.io/', files=files)
+        if response.status_code==200:
+            data=response.json()
+            link=data["link"]
+            entry.insert(0, link)
 
 window=Tk()
 window.title("Файл в облако")
