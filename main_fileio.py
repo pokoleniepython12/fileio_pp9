9c19ed81480971ab0b11c2f052e63c66b2e3629e
Екатерина <katrindenev23@yandex.com>
2024-11-26 10:30:30 +0300

    Добавленa возможность сохранять ссылки на файлы в формате json.

--- main_fileio.py
+++ main_fileio.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import os
+import json
 import requests
 import pyperclip
 from tkinter import filedialog as fd
@@ -18,11 +20,28 @@ def upload():
                 pyperclip.copy(link)
                 entry.delete(0,END)
                 entry.insert(0, link)
+                save_history(filepath, link)
                 mb.showinfo("Успешно","Ссылка успешно скопирована в буфер обмена")
     except KeyError:
         mb.showerror("Ошибка!","Невозможно получить ссылку")
     except Exception as e:
         mb.showerror("Ошибка",e)
+
+def save_history(filepath, link):
+    history=[]
+
+    if os.path.exists(history_file):
+        with open(history_file, "r") as f:
+            history=json.load(f)
+
+    history.append({"file_path":os.path.basename(filepath),
+                    "download_link":link})
+
+    with open(history_file, "w") as f:
+        json.dump(history, f, indent=4)
+
+
+history_file = "upload_history.json"
 
 window=Tk()
 window.title("Файл в облако")
